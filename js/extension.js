cdab0b80bd052761d9ba09af3a17617c932436c0
harchica <ch0.hi.l@outlook.jp>
2018-06-21 13:15:17 +0900

    linkpage作成済

--- js/extension.js
+++ js/extension.js
@@ -6,12 +6,14 @@ $(function(){
     $("#first").click(function(){
         // 「id="jQueryBox"」の表示、非表示を切り替える
         $("#hidden_cap").toggle();
-        $("#fake").css("padding", "80px");
+        $("#fake").css("padding-top", "0px");
+        $("#fake").css("padding-bottom", "100px");
+
         $("#first").toggle();
     });
     $("#reduction").click(function(){
         $("#first").toggle();
-        $("#fake").css("padding","120px 20px 20px 20px");
+        $("#fake").css("padding","80px 20px 20px 20px");
         $("#hidden_cap").toggle();
     });
 });
